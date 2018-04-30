e1bd8a0a46f26c47e0c784d291e48347a034642d
Ryan Stonebraker <ryanastonebraker@gmail.com>
2018-04-29 22:08:45 -0800

    Adds looping and comments code.

--- bin/randomSynonym.dart
+++ bin/randomSynonym.dart
@@ -7,11 +7,14 @@ import "dart:io";
 import 'dart:math';
 import 'package:http/http.dart' as http;
 
-List getDelimitedList(String rawString, String delimiter) {
+
+// Splits a raw string by newlines and then removes up to the end of a marker
+// in each string.
+List getScrubbedList(String rawString, String marker) {
   var delimitedList = rawString.split("\n");
   for (var i = 0; i < delimitedList.length; ++i) {
-    if (delimitedList[i].length > delimiter.length) {
-      int delimiterEnd = delimitedList[i].indexOf(delimiter) + delimiter.length;
+    if (delimitedList[i].length > marker.length) {
+      int delimiterEnd = delimitedList[i].indexOf(marker) + marker.length;
       delimitedList[i] = delimitedList[i].substring(delimiterEnd);
     }
     else {
@@ -21,42 +24,61 @@ List getDelimitedList(String rawString, String delimiter) {
   return delimitedList;
 }
 
+
+// Prints a random synonym from the bighugelabs thesaurus response page.
 void printRandomSyn(dynamic syns) {
-  syns = getDelimitedList(syns, "syn|");
+  syns = getScrubbedList(syns, "syn|");
   var randGen = new Random();
   var randomSyn = syns[randGen.nextInt(syns.length)];
   print ("Random Synonym: " + randomSyn);
 }
 
+
+// Handles errors with get request, prints error, and recurses through main
 void pageLoadError(error) {
-  print ("Received the following error: ");
-  print (error);
-  print ("Try Again!");
+  stderr.write("Received the following error: ");
+  stderr.write(error);
+  stderr.write("\nTry Again!\n\n");
   main();
 }
 
+
+// Gets input from the user consisting only of alphabetical characters
 String getSanitizedInput() {
   stdout.write ("Please Enter A Word: ");
   var userWord = stdin.readLineSync();
+  if (userWord.length == 0)
+    exit(0);
   userWord = userWord.toLowerCase();
-
   for (var i = 0; i < userWord.length; ++i) {
     if (userWord.codeUnitAt(i) < 'a'.codeUnits[0] || userWord.codeUnitAt(i) > 'z'.codeUnits[0]) {
-      print("Bad Entry! Retry!");
+      stderr.write("Bad Entry! Retry!\n");
       userWord = getSanitizedInput();
       break;
     }
   }
-
   return userWord;
 }
 
-void main() {
+
+// Gets an alphabetical word from user and uses the dart http library to find
+// a synonym using a thesaurus api. The plaintext response or error is then
+// handled appropriately. The program then loops.
+getRandomSynonym() async {
   var userWord = getSanitizedInput();
   var thesaurusBaseURL = "http://words.bighugelabs.com/api/2";
   var apiKey = "1166c33a777038684549d7422cc054fc";
 
-  var fullUrl = "${thesaurusBaseURL}/${apiKey}/${userWord}/";
+  var fullUrl = "$thesaurusBaseURL/$apiKey/$userWord/";
 
-  http.read(fullUrl).then(printRandomSyn).catchError(pageLoadError);
+  await http.read(fullUrl).then(printRandomSyn).catchError(pageLoadError);
+}
+
+
+// Loops getting a random synonym for a user entered word.
+main() async {
+  while (true) {
+    await getRandomSynonym();
+    stdout.write("\nNewline to Exit.\n");
+  }
 }
